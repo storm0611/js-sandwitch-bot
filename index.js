f565674942a251eb3f38775a37fdc3f95b863fdf
storm0611 <devstar0611@gmail.com>
2024-03-06 14:37:26 -0500

    update

--- index.js
+++ index.js
@@ -18,28 +18,27 @@ const main = async () => {
     nonce: 0,
     type: 2
   }
-  console.log(tx)
-    if (tx == null) {
-      return;
-    }
-    const routerDataDecoded = await parseUniv2RouterTx(tx);
-    if (routerDataDecoded == null) {
-      return;
-    }
-    const { path, userAmountIn, amountOutMin } = routerDataDecoded;
-    if (path.length == 2) {
-      const fromToken = path[0];
-      const toToken = path[1];
-      const pair = getUniv2PairAddress(fromToken, toToken);
-      const [reserveFrom, reserveTo] = await getUniv2Reserve(pair, fromToken, toToken);
-      console.log(reserveFrom, reserveTo);
-      // const { amountIn, newReserveFrom, newReserveTo } = getUniv2DataGivenOut(
-      //   amountOutMin,
-      //   reserveFrom,
-      //   reserveTo
-      // );
-      // console.log(amountIn, newReserveFrom, newReserveTo);
-    }
+  if (tx == null) {
+    return;
+  }
+  const routerDataDecoded = await parseUniv2RouterTx(tx);
+  if (routerDataDecoded == null) {
+    return;
+  }
+  const { path, userAmountIn, amountOutMin } = routerDataDecoded;
+  console.log(path, userAmountIn, amountOutMin)
+  if (path.length == 2) {
+    const fromToken = path[0];
+    const toToken = path[1];
+    const pair = getUniv2PairAddress(fromToken, toToken);
+    const [reserveFrom, reserveTo] = await getUniv2Reserve(pair, fromToken, toToken);
+    // const { amountIn, newReserveFrom, newReserveTo } = getUniv2DataGivenOut(
+    //   amountOutMin,
+    //   reserveFrom,
+    //   reserveTo
+    // );
+    // console.log(amountIn, newReserveFrom, newReserveTo);
+  }
   // });
 };
 
